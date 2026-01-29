f2f8365eaf9960afa099dd8071a6756f4c6d36b4
chez1s <71917288+TRIBUI106@users.noreply.github.com>
2026-01-29 15:47:26 +0700

    done: lab1

--- Lab6/src/views/bai1.vue
+++ Lab6/src/views/bai1.vue
@@ -1,23 +1,25 @@
 <template>
-    <div>
-        <h1 class="mt-5">Bài 1</h1>
-        <h2>{{ message }}</h2>
 
-        <div class="btn btn-primary" @click="updateMessage">
-            Thay doi thong bao
+    <div class="container mt-5">
+        <span>Nhập điểm của bạn : </span>
+        <input type="number" class="rounded px-2" v-model="num" min="0" max="10" step="0.25">
+
+        <div v-if="num != 0">
+            <span class="fw-bold me-2">Hạnh kiểm:</span>
+            <span class="text-danger" v-if="num < 5">Yếu</span>
+            <span class="text-warning" v-else-if="num < 6.5">Trung bình</span>
+            <span class="text-warning" v-else-if="num < 8">Khá</span>
+            <span class="text-success" v-else-if="num < 9">Giỏi</span>
+            <span class="text-success" v-else>Xuất sắc</span>
         </div>
     </div>
+
 </template>
 
 <script setup>
+
 import { ref } from 'vue';
 
-const message = ref('first message');
-
-const updateMessage = () => {
-    message.value = message.value === 'first message' ? 'second message' : 'first message';
-}
+const num = ref(0)
 
 </script>
-
-<style scoped></style>
